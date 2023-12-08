4f6043a486fc766ea6f45d5223143359f7b63540
JustABitOff <cameronjcyr@gmail.com>
2023-12-07 18:54:00 -0600

    add tito message

---
+++ message_slack_tito.py
@@ -0,0 +1,46 @@
+from collections import Counter
+import json
+import os
+import requests
+
+from pendulum import today
+from slack_sdk import WebClient
+from slack_sdk.errors import SlackApiError
+
+client = WebClient(token=os.getenv('SLACK_TOKEN'))
+
+def get_ticket_sales(url):
+    try:
+        response = requests.get(
+            url,
+            headers={
+                'Accept': 'application/json',
+                'Authorization': f'Token token={os.getenv("TITO_TOKEN")}'
+            }
+        )
+
+        if response.status_code == 200:
+            data_dict = json.loads(response.text)
+            return data_dict
+        else:
+            response.raise_for_status()
+
+    except requests.exceptions.RequestException as e:
+        print(f"Error: {e}")
+        return None
+    
+
+response = get_ticket_sales('https://api.tito.io/v3/datatuneconf/nashville-2024/tickets')
+registration_counts = Counter(i['release_title'] for i in response['tickets'])
+message = '\n'.join([i + ': ' + str(registration_counts[i]) for i in registration_counts])
+
+channel_id = "C05L51TPK7T"
+
+try:
+    result = client.chat_postMessage(
+        channel=channel_id,
+        text=f':admission_tickets: Ticket sales breakdown as of {today().format("MMMM DD, YYYY")} :admission_tickets:\n\n{message}'
+    )
+
+except SlackApiError as e:
+    print(f"Error: {e}")
